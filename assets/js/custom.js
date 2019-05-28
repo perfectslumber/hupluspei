85c677ad4bb14bbb7b87a182bd31b298b9485a12
KEXIN PEI <kexinpei@KEXINs-MacBook-Pro.local>
2019-05-28 18:43:13 -0400

    live

---
+++ assets/js/custom.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+	// Add smooth scrolling to all links
+	$("a").on('click', function (event) {
+
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
+
+			// Store hash
+			var hash = this.hash;
+
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 100, function () {
+
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			});
+		} // End if
+	});
+});
+
+function like(x) {
+	var y = x.nextElementSibling;
+	if (x.classList.contains("text-300")) {
+		x.classList.replace("text-300", "text-danger");
+		y.innerHTML = Number(y.innerHTML) + 1;
+	}
+	else{
+		x.classList.replace("text-danger", "text-300");
+		y.innerHTML = Number(y.innerHTML) - 1;
+	}
+}
